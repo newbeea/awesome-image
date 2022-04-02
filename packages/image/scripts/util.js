7cc0910db4bafd7a3e0a11c6a062b0ccbb2d3ffc
xupengfei <xupengfei2010@gmail.com>
2022-04-02 19:57:35 +0800

    chore: ignore dist

--- packages/image/scripts/util.js
+++ packages/image/scripts/util.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 function copy(src, dest) {
   try {
+    fs.unlinkSync(dest)
+  }
+  catch (error) { }
+  try {
     const content = fs.readFileSync(src, 'utf-8')
     fs.writeFileSync(dest, content, 'utf-8')
   }
